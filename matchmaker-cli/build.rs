8e7bd4e526009e637840b092a6a8dc1bc66c798d
squirreljetpacks <squirreljetpacks@gmail.com>
2026-05-26 19:45:32 -0400

    Release 0.0.35

--- matchmaker-cli/build.rs
+++ matchmaker-cli/build.rs
@@ -9,20 +9,22 @@ use std::env;
 // -----------------------------------------------------------------------------
 include!("src/clap.rs");
 
-fn main() {
-    println!("cargo:rerun-if-changed=src/cli/types.rs");
-    println!("cargo:rerun-if-changed=build.rs");
-
-    let out_dir = {
-        let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
-        let out_dir = manifest_dir.join("assets").join("completions");
-        std::fs::create_dir_all(&out_dir).unwrap();
-        out_dir
-    };
-
+fn generate_completions() {
+    let manifest_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
+    let out_dir = manifest_dir.join("assets").join("completions");
+    std::fs::create_dir_all(&out_dir).unwrap();
+    
     let mut cmd = Cli::command();
-
     for shell in [Shell::Bash, Shell::Zsh, Shell::Fish, Shell::PowerShell] {
         generate_to(shell, &mut cmd, BINARY_SHORT, &out_dir).unwrap();
+    }
+}
+
+fn main() {
+    println!("cargo:rerun-if-changed=src/clap.rs");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    if env::var_os("SKIP_COMPLETIONS").is_none() {
+        generate_completions();
     }
 }
